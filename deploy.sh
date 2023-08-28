e70ec2c1981c6244e37431973c462ce293a1f962
unknown <git config --global user.em>
2023-08-28 21:50:40 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,9 @@ cd dist
 git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
 git add -A
 git commit -m 'deploy'
-# 部署到 https://github.com/crwind/TestVueWeb 分支為 gh-pages
-git push -f https://github.com/crwind/TestVueWeb master:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-cd -
+
+# 部署到 https://crwind.github.io
+# git push -f git@github.com:crwind/crwind.github.io.git master
+
+# 部署到 https://crwind.github.io/<REPO>
+git push -f git@github.com:crwind/TestVueWeb.git master:gh-pages
