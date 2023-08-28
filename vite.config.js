39e07846141e3b002aabe23194aac49312fc1903
unknown <git config --global user.em>
2023-08-28 23:59:24 +0800

    test 1

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/TestVueWeb/"
+  base: "/TestVueWeb/",
+  build: {
+    outDir: "docs"
+  }
 })
 
